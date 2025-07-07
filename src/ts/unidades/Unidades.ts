75bc534993d0cddb95477435b293c783ee4f256c
fabianmolinab <molinafabian11b@gmail.com>
2025-07-07 10:58:00 -0500

    feature(): add calculoAtaqueMaximo() and testing method

--- src/ts/unidades/Unidades.ts
+++ src/ts/unidades/Unidades.ts
@@ -1,10 +1,26 @@
 export class Unidades {
     private vidaMaxima: number;
+    private ataqueMaximo: number;
     private valorPorcentajeVida1: number;
     private valorPorcentajeVida2: number;
 
-    constructor(vidaMaxima: number, valorPorcentajeVida1: number, valorPorcentajeVida2: number) {
+    constructor(
+        vidaMaxima: number,
+        ataqueMaximo: number,
+        valorPorcentajeVida1: number,
+        valorPorcentajeVida2: number,
+    ) {
+        if (vidaMaxima <= 0)
+            throw new Error("La vida maxima debe ser positiva");
+        if (ataqueMaximo <= 0)
+            throw new Error("El ataque maximo deber ser positivo");
+        if (valorPorcentajeVida1 <= 0)
+            throw new Error("valorPorcentajeVida debe ser positivo");
+        if (valorPorcentajeVida2 <= 0)
+            throw new Error("valorPorcentajeVida2 debe ser positivo");
+
         this.vidaMaxima = vidaMaxima;
+        this.ataqueMaximo = ataqueMaximo;
         this.valorPorcentajeVida1 = valorPorcentajeVida1;
         this.valorPorcentajeVida2 = valorPorcentajeVida2;
     }
@@ -17,12 +33,31 @@ export class Unidades {
         this.vidaMaxima = vidaMaxima;
     }
 
-    calculoPorcentajeVidaUnidad(): number {
+    protected calculoPorcentajeVidaUnidad(): number {
         return this.valorPorcentajeVida1 / this.valorPorcentajeVida2;
     }
 
     calculoVidaMaxima(): number {
         this.vidaMaxima = this.vidaMaxima * this.calculoPorcentajeVidaUnidad();
         return this.vidaMaxima;
+    }
+
+    calculoAtaqueMaximo(
+        ataqueBase: number,
+        valorAtaque1: number,
+        valorAtaque2: number,
+    ): number {
+        if (ataqueBase < 0) {
+            throw new Error("los parametros no pueden ser negativos");
+        }
+        if (valorAtaque1 < 0) {
+            throw new Error("valorAtaque1 no pueden ser negativos");
+        }
+        if (valorAtaque2 < 0) {
+            throw new Error("valorAtaque2 no pueden ser negativos");
+        }
+        this.ataqueMaximo = (valorAtaque1 / valorAtaque2) * ataqueBase;
+
+        return this.ataqueMaximo;
     }
 }
--- tests/unidades/unidades.test.ts
+++ tests/unidades/unidades.test.ts
@@ -1,13 +1,13 @@
-import { describe, test, beforeEach, expect } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import { Unidades } from "../../src/ts/unidades/Unidades";
 
 describe("Unidades", () => {
     let unidades: Unidades;
     beforeEach(() => {
-        unidades = new Unidades(3400, 2, 10);
+        unidades = new Unidades(3400, 500, 2, 10);
     });
 
-    test("comprueba que la clase Unidades sea un function", () => {
+    test("comprueba que la clase Unidades exista debe retornar que es tipo function", () => {
         expect(Unidades).toBeTypeOf("function");
     });
 
@@ -15,8 +15,12 @@ describe("Unidades", () => {
         expect(unidades).toBeTypeOf("object");
     });
 
-    test("comprobar que Unidades tenga como atributo VidaMaxima", () => {
+    test("comprobar que Unidades tenga como propiedad VidaMaxima", () => {
         expect(unidades).toHaveProperty("vidaMaxima");
+    });
+
+    test("comprobar que Unidades tenga como propiedad AtaqueMaximo", () => {
+        expect(unidades).toHaveProperty("ataqueMaximo");
     });
 
     test("comprobar el calculo de vida maxima retorne un numero", () => {
@@ -25,30 +29,78 @@ describe("Unidades", () => {
     });
 
     /*test('debería [acción/resultado esperado] cuando [condición/contexto]', () => {});*/
-    test("deberia calcular la vida maxima de un soldado regular 2500", () => {
-        //Soldados Regulares
-        let soldadosRegulares: Unidades;
-        soldadosRegulares = new Unidades(2500, 2, 12);
+    describe("calculo vida maxima", () => {
+        test("deberia calcular la vida maxima de un soldado regular 2500", () => {
+            let soldadosRegulares: Unidades;
+            soldadosRegulares = new Unidades(2500, 500, 2, 12);
 
-        const porcentaje = 2 / 12;
-        const valorinicial = 2500;
+            const porcentaje = 2 / 12;
+            const valorinicial = 2500;
 
-        const respuesta: number = porcentaje * valorinicial;
+            const respuesta: number = porcentaje * valorinicial;
 
-        expect(soldadosRegulares.calculoVidaMaxima()).toBe(respuesta);
+            expect(soldadosRegulares.calculoVidaMaxima()).toBe(respuesta);
+        });
+
+        test("deberia calcular la vida maxima de un soldado profesional de 2500 con 3/12", () => {
+            const soldadoProfesional: Unidades = new Unidades(2500, 500, 3, 12);
+            const procentajeProfesionales = 3 / 12;
+            const valorInicialProfesionales = 2500;
+
+            const respuestaSoldadosProfesionales: number =
+                procentajeProfesionales * valorInicialProfesionales;
+
+            expect(soldadoProfesional.calculoVidaMaxima()).toBe(
+                respuestaSoldadosProfesionales,
+            );
+        });
+
+        test("deberia mostrar un error cuando se le ingresa numeros negativos en el valor de la vida maxima", () => {
+            expect(() => new Unidades(-2500, 500, 3, 12)).toThrow(
+                "La vida maxima debe ser positiva",
+            );
+        });
     });
+    describe("calculo ataque maximo ", () => {
+        test("deberia comprobar que calculoAtaqueMaximo() retorne un tipo number", () => {
+            let calculo = unidades.calculoAtaqueMaximo(1, 2, 3);
+            expect(calculo).toBeTypeOf("number");
+        });
 
-    test("deberia calcular la vida maxima de un soldado regular 2500", () => {
-        //Soldados Profesionales
-        const soldadoProfesional: Unidades = new Unidades(2500, 3, 12);
-        const procentajeProfesionales = 3 / 12;
-        const valorInicialProfesionales = 2500;
+        test("deberia retornar error si ataqueBase es <0 cuando el numero es negativo ", () => {
+            expect(() => unidades.calculoAtaqueMaximo(-1000, 2, 1)).toThrow();
+        });
 
-        const respuestaSoldadosProfesionales: number =
-            procentajeProfesionales * valorInicialProfesionales;
+        test("validar el error si valorAtaque1 es negativo ", () => {
+            expect(() => unidades.calculoAtaqueMaximo(1000, -2, 1)).toThrow();
+        });
 
-        expect(soldadoProfesional.calculoVidaMaxima()).toBe(
-            respuestaSoldadosProfesionales,
-        );
+        test("validar el error si valorAtaque2 es negativo ", () => {
+            expect(() => unidades.calculoAtaqueMaximo(1000, 2, -1)).toThrow();
+        });
+
+        test("deberia calcular calculoAtaqueMaximo() el ataque Maximo de un soldado regular que tiene como valor 1/14 de un submarino con valor 500", () => {
+            let validarAtaque: Unidades = new Unidades(1, 1, 1, 1);
+            const calculo: number = (1 / 14) * 500;
+
+            expect(validarAtaque.calculoAtaqueMaximo(500, 1, 14)).toBe(calculo);
+        });
+
+        test("deberia calcular calculoAtaqueMaximo() el ataque Maximo de un soldado profesional que tiene como valor 2/14 de un submarino con valor 500", () => {
+            let validarAtaque: Unidades = new Unidades(1, 500, 1, 1);
+            const calculo: number = (2 / 14) * 500;
+
+            expect(validarAtaque.calculoAtaqueMaximo(500, 2, 14)).toBe(calculo);
+        });
+
+        test("deberia calcular calculoAtaqueMaximo() el ataque Maximo de un soldado profesional que tiene como valor 2/14 como valor base aleatorio", () => {
+            let validarAtaque: Unidades = new Unidades(1, 1, 1, 1); //inicialización
+            let numeroBase: number = Math.random() * (500 - 1000) + 1000;
+            const calculo: number = (2 / 14) * numeroBase; //operación
+
+            expect(validarAtaque.calculoAtaqueMaximo(numeroBase, 2, 14)).toBe(
+                calculo,
+            );
+        });
     });
 });
